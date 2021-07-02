7e2a3a2c7f3856e87edba746fa766726049613a9
Paul Radic <paul.radic@epita.fr>
2021-07-02 17:02:40 +0200

    core(service): add mission service without method

--- src/main/java/fr/epita/socra/domain/service/MissionService.java
+++ src/main/java/fr/epita/socra/domain/service/MissionService.java
@@ -1,2 +1,13 @@
-package fr.epita.socra.domain.service;public class MissionService {
+package fr.epita.socra.domain.service;
+
+import fr.epita.socra.domain.entity.MissionEntity;
+
+import java.util.*;
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class MissionService {
+    public List<MissionEntity> findAllMission() {
+        return null;
+    }
 }
