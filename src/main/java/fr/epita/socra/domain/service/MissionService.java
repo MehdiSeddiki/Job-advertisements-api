c7cb63a20f5806f4e08a2e6de1f832373664f86e
Paul Radic <paul.radic@epita.fr>
2021-07-02 17:50:21 +0200

    core(service): add methods

--- src/main/java/fr/epita/socra/domain/service/MissionService.java
+++ src/main/java/fr/epita/socra/domain/service/MissionService.java
@@ -1,6 +1,7 @@
 package fr.epita.socra.domain.service;
 
 import fr.epita.socra.domain.entity.MissionEntity;
+import fr.epita.socra.presentation.mission.*;
 
 import java.util.*;
 import javax.enterprise.context.ApplicationScoped;
@@ -8,6 +9,22 @@ import javax.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class MissionService {
     public List<MissionEntity> findAllMission() {
+        return null;
+    }
+    public MissionEntity findOneMission(long missionId)
+    {
+        return null;
+    }
+    public List<MissionEntity> searchMission(String search)
+    {
+        return null;
+    }
+    public MissionEntity createMission(CreateMissionRequest mission)
+    {
+        return null;
+    }
+    public MissionEntity updateMission(UpdateMissionRequest mission)
+    {
         return null;
     }
 }
