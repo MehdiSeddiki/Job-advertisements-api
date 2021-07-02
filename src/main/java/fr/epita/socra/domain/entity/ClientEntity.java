f9361767d3042f24384d05f26c6696e6b0e63b7c
Alexia Becouze <alexia.becouze@epita.fr>
2021-07-02 15:36:18 +0200

    feat(entities): add entities classes
    Signed-off-by: Alexia Becouze <alexia.becouze@epita.fr>

--- src/main/java/fr/epita/socra/domain/entity/ClientEntity.java
+++ src/main/java/fr/epita/socra/domain/entity/ClientEntity.java
@@ -1,4 +1,15 @@
 package fr.epita.socra.domain.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class ClientEntity {
+    private long id;
+    private String name;
 }
--- src/main/java/fr/epita/socra/domain/entity/MissionEntity.java
+++ src/main/java/fr/epita/socra/domain/entity/MissionEntity.java
@@ -1,4 +1,25 @@
 package fr.epita.socra.domain.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class MissionEntity {
+    long id;
+    String location;
+    int duration;
+    int price;
+    float remote;
+    Date beginning;
+    String job;
+    String context;
+    String description;
+    long clientId;
 }
