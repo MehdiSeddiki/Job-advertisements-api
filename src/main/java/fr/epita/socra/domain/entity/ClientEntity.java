c2e856eddd3e6134d8d755a3b95ebe0634bb615e
Alexia Becouze <alexia.becouze@epita.fr>
2021-07-02 18:30:56 +0200

    chore(entity): change entity to java records
    Signed-off-by: Alexia Becouze <alexia.becouze@epita.fr>

--- src/main/java/fr/epita/socra/domain/entity/ClientEntity.java
+++ src/main/java/fr/epita/socra/domain/entity/ClientEntity.java
@@ -1,15 +1,6 @@
 package fr.epita.socra.domain.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-public class ClientEntity {
-    private long id;
-    private String name;
+public record ClientEntity(
+        long id,
+        String name) {
 }
--- src/main/java/fr/epita/socra/domain/entity/MissionEntity.java
+++ src/main/java/fr/epita/socra/domain/entity/MissionEntity.java
@@ -1,25 +1,15 @@
 package fr.epita.socra.domain.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.util.Date;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-public class MissionEntity {
-    long id;
-    String location;
-    int duration;
-    int price;
-    float remote;
-    Date beginning;
-    String job;
-    String context;
-    String description;
-    long clientId;
+public record MissionEntity(
+        String location,
+        int duration,
+        int price,
+        float remote,
+        Date beginning,
+        String job,
+        String context,
+        String description,
+        long clientId) {
 }
