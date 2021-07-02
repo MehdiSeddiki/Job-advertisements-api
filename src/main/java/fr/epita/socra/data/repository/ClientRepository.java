1f1d1284ff1c772b59bda8c2a07150d1d2de208d
Paul Radic <paul.radic@epita.fr>
2021-07-02 14:37:05 +0200

    core(repository): add Mission and Client Repository

---
+++ src/main/java/fr/epita/socra/data/repository/ClientRepository.java
@@ -0,0 +1,7 @@
+package fr.epita.socra.data.repository;
+
+import fr.epita.socra.data.model.Client;
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+
+public class ClientRepository implements PanacheRepositoryBase<Client, Long> {
+}
